b61913fe7614a84b2ac5c52233fb743f29e720b7
Hernan Carvajal <carvajal.hernandavid@gmail.com>
2025-06-07 23:29:34 +0200

    feat: geting results from github API

--- Frontend/github-user-search/src/App.tsx
+++ Frontend/github-user-search/src/App.tsx
@@ -1,67 +1,50 @@
+import { useState, type ChangeEvent } from 'react';
 import './App.css';
 
-const users = [
-  {
-    login: 'hernan',
-    id: 15841,
-    avatar_url: 'https://avatars.githubusercontent.com/u/15841?v=4',
-    url: 'https://api.github.com/users/hernan',
-  },
-  {
-    login: 'bodymovin',
-    id: 11096607,
-    avatar_url: 'https://avatars.githubusercontent.com/u/11096607?v=4',
-    url: 'https://api.github.com/users/bodymovin',
-  },
-  {
-    login: 'hernandev',
-    id: 1143355,
-    avatar_url: 'https://avatars.githubusercontent.com/u/1143355?v=4',
-    url: 'https://api.github.com/users/hernandev',
-  },
-  {
-    login: 'hernan604',
-    id: 480616,
-    avatar_url: 'https://avatars.githubusercontent.com/u/480616?v=4',
-    url: 'https://api.github.com/users/hernan604',
-  },
-  {
-    login: 'hernan066',
-    id: 30220961,
-    avatar_url: 'https://avatars.githubusercontent.com/u/30220961?v=4',
-    url: 'https://api.github.com/users/hernan066',
-  },
-  {
-    login: 'hgrecco',
-    id: 278566,
-    avatar_url: 'https://avatars.githubusercontent.com/u/278566?v=4',
-    url: 'https://api.github.com/users/hgrecco',
-  },
-  {
-    login: 'hernandw',
-    id: 43607368,
-    avatar_url: 'https://avatars.githubusercontent.com/u/43607368?v=4',
-    url: 'https://api.github.com/users/hernandw',
-  },
-  {
-    login: 'hernandoabella',
-    id: 24196857,
-    avatar_url: 'https://avatars.githubusercontent.com/u/24196857?v=4',
-    url: 'https://api.github.com/users/hernandoabella',
-  },
-];
-
 function App() {
+  const [userQuery, setUserQuery] = useState('');
+  const [users, setUsers] = useState([]);
+
+  const handleQueryChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const newQueryValue = event.target.value;
+    setUserQuery(newQueryValue);
+
+    if (newQueryValue === '') {
+      setUsers([]);
+      return;
+    }
+
+    const params = new URLSearchParams();
+    params.append('q', newQueryValue);
+
+    fetch(`https://api.github.com/search/users?${params}`)
+      .then((response) => response.json())
+      .then((data) => setUsers(data.items));
+  };
+
+  const noUserQuery = userQuery === '';
+  const noResults = userQuery !== '' && users.length === 0;
+
   return (
     <>
       <header>Github search</header>
       <main>
         <section id="search">
-          <input type="text" placeholder="Search input" />
+          <input
+            type="text"
+            placeholder="Search input"
+            onChange={handleQueryChange}
+            value={userQuery}
+          />
         </section>
+
         <section id="results">
+          {noUserQuery && <div>Start typing to search</div>}
+          {noResults && <div>There were no users found by '{userQuery}'</div>}
           {users.map((user) => (
-            <article className="card-user">{user.login}</article>
+            <article key={user.id} className="card-user">
+              {user.login}
+            </article>
           ))}
         </section>
       </main>
