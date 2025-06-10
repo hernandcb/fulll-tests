22d923698d0afc305316d89a7bd48eaad35ec02e
Hernan Carvajal <carvajal.hernandavid@gmail.com>
2025-06-10 16:09:55 +0200

    feat: add testing setup and UserCard component tests

---
+++ Frontend/github-user-search/src/components/UserCard/UserCard.test.tsx
@@ -0,0 +1,31 @@
+import { render, screen } from '@testing-library/react';
+import type { User } from '../../types/User';
+import { UserCard } from './UserCard';
+
+describe('Test UserCard component', () => {
+  it('renders UserCard component', () => {
+    // Prepare mock data
+    const mockUser: User = {
+      login: 'testuser',
+      id: 12345,
+      avatar_url: 'https://example.com/avatar.jpg',
+      url: 'github.com/testuser',
+    };
+
+    // Render the UserCard component
+    render(<UserCard user={mockUser} />);
+
+    // Check if the data is displayed correctly
+
+    expect(screen.getByRole('img')).toBeInTheDocument();
+    expect(
+      screen.getByAltText(`Avatar of ${mockUser.login}`),
+    ).toBeInTheDocument();
+    expect(screen.getByText(mockUser.id.toString())).toBeInTheDocument();
+    expect(screen.getByText(mockUser.login)).toBeInTheDocument();
+    expect(screen.getByRole('link')).toHaveAttribute(
+      'href',
+      `https://github.com/${mockUser.login}`,
+    );
+  });
+});
