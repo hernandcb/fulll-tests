c47a9106017fb6ed887a5e1d221bc57cc1dc7051
Hernan Carvajal <carvajal.hernandavid@gmail.com>
2025-06-10 20:34:44 +0200

    feat: implement card selection with count and select-all functionality

--- Frontend/github-user-search/src/App.tsx
+++ Frontend/github-user-search/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, type ChangeEvent } from 'react';
+import { useEffect, useState, type ChangeEvent } from 'react';
 import { UserCard } from './components/UserCard';
 import { useDebounce } from './hooks/useDebounce';
 import { useGithubSearch } from './hooks/useGithubSearch';
@@ -8,7 +8,17 @@ import { StateMessage } from './components/StateMessage';
 function App() {
   const [userQuery, setUserQuery] = useState<string>('');
   const { users, loading, apiLimitExceeded, searchUsers } = useGithubSearch();
+  const [selectedUserIDs, setSelectedUserIDs] = useState<
+    Record<number, boolean>
+  >({});
+  const [allSelected, setAllSelected] = useState<boolean>(false);
   const debouncedSendQuery = useDebounce(searchUsers, 500);
+
+  useEffect(() => {
+    // Reset selected users when the user query changes
+    setSelectedUserIDs({});
+    setAllSelected(false);
+  }, [users]);
 
   const handleQueryChange = async (event: ChangeEvent<HTMLInputElement>) => {
     const newQueryValue = event.target.value;
@@ -16,6 +26,30 @@ function App() {
     debouncedSendQuery(newQueryValue);
   };
 
+  const toogleCardSelection = (userId: number) => {
+    setSelectedUserIDs((prevSelected) => ({
+      ...prevSelected,
+      [userId]: !prevSelected[userId],
+    }));
+  };
+
+  const handleSelectAllChange = () => {
+    const newSelectionState = !allSelected;
+    setAllSelected(newSelectionState);
+    setSelectedUserIDs(
+      users.reduce(
+        (acc, user) => ({ ...acc, [user.id]: newSelectionState }),
+        {},
+      ),
+    );
+  };
+
+  const numberOfCardsSelected = Object.values(selectedUserIDs).reduce(
+    (count, isSelected) => count + (isSelected ? 1 : 0),
+    0,
+  );
+
+  console.log('Number of cards selected:', numberOfCardsSelected);
   return (
     <div className="app">
       <header>Github search</header>
@@ -28,6 +62,22 @@ function App() {
         />
       </section>
 
+      {users.length > 0 && (
+        <section className="actions">
+          <div className="selected-count">
+            <input
+              type="checkbox"
+              checked={allSelected}
+              onChange={handleSelectAllChange}
+            />
+            {numberOfCardsSelected > 0
+              ? `Selected ${numberOfCardsSelected} user${numberOfCardsSelected > 1 ? 's' : ''}`
+              : 'No users selected'}
+          </div>
+          <div className="options"></div>
+        </section>
+      )}
+
       <section className="results">
         <StateMessage
           loading={loading}
@@ -37,7 +87,12 @@ function App() {
         />
 
         {users.map((user) => (
-          <UserCard key={user.id} user={user} />
+          <UserCard
+            key={user.id}
+            user={user}
+            selected={selectedUserIDs[user.id]}
+            toogleSelection={toogleCardSelection}
+          />
         ))}
       </section>
     </div>
